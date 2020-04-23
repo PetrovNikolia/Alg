63e1738b7270dbb4681348c36933279e8f70b9de
PetrovNikolia <kintep84@mail.ru>
2020-04-23 15:57:54 +0500

    DZ6

---
+++ src/Lesson6/Main.java
@@ -0,0 +1,24 @@
+package Lesson6;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        balance();
+    }
+    static void balance() {
+        int balanced = 0;
+        Random random = new Random();
+        Tree<Integer, String>[] treeMaps = new Tree[10000];
+        for (int i = 0; i < treeMaps.length; i++) {
+            treeMaps[i] = new Tree<>();
+            while (treeMaps[i].height() < 5) {
+                treeMaps[i].put(random.nextInt(200) , "");
+            }
+            if (treeMaps[i].isBalance()) {
+                balanced++;
+            }
+        }
+        System.out.println("\n" + balanced);
+    }
+}
